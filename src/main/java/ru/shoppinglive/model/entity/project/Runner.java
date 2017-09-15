cc8950dfcf4a02a09057d955bb87039b4592a79e
Roman Khabibullin <a-xp@ya.ru>
2017-09-15 18:32:31 +0300

    builds 2

---
+++ src/main/java/ru/shoppinglive/model/entity/project/Runner.java
@@ -0,0 +1,12 @@
+package ru.shoppinglive.model.entity.project;
+
+import lombok.Data;
+
+/**
+ * Created by rkhabibullin on 15.09.2017.
+ */
+@Data
+public class Runner {
+    public String id;
+    public String hostName;
+}
--- src/main/java/ru/shoppinglive/model/service/remote/RunnerService.java
+++ src/main/java/ru/shoppinglive/model/service/remote/RunnerService.java
@@ -1,13 +1,20 @@
 package ru.shoppinglive.model.service.remote;
 
+import ru.shoppinglive.model.entity.project.Runner;
+
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by rkhabibullin on 15.09.2017.
  */
 public class RunnerService {
 
 
+    public List<Runner> getRunners(){
 
-
+        return Collections.emptyList();
+    }
 
 
 
